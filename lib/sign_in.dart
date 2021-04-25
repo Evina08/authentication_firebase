af119f0ba7f03262719f5d4415c37fb834763fc9
Evina <64392104+Evina08@users.noreply.github.com>
2021-04-25 13:53:34 +0700

    Praktikum + tugas

---
+++ lib/sign_in.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+String name;
+String email;
+String imageUrl;
+Future<String> signInWithGoogle() async {
+  await Firebase.initializeApp();
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+  final UserCredential authResult =
+      await _auth.signInWithCredential(credential);
+  final User user = authResult.user;
+  if (user != null) {
+    // Checking if email and name is null
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(user.photoURL != null);
+    name = user.displayName;
+    email = user.email;
+    imageUrl = user.photoURL;
+    // Only taking the first part of the name, i.e., First Name
+    if (name.contains(" ")) {
+      name = name.substring(0, name.indexOf(" "));
+    }
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+    final User currentUser = _auth.currentUser;
+    assert(user.uid == currentUser.uid);
+    print('signInWithGoogle succeeded: $user');
+    return '$user';
+  }
+  return null;
+}
+
+Future<void> signOutGoogle() async {
+  await googleSignIn.signOut();
+  print("Sign Out");
+}
+
+Future<User> signInWithEmail() async {
+  await Firebase.initializeApp();
+  User user;
+  try {
+    UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+      email: 'evinadindasyawalia@gmail.com',
+      password: 'evina080102',
+    );
+    user = userCredential.user;
+
+    if (user != null) {
+      name = 'Evina';
+      email = user.email;
+    }
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'user-not-found') {
+      print('No user found for that email.');
+    } else if (e.code == 'wrong-password') {
+      print('Wrong password provided.');
+    }
+  }
+  return user;
+}
+
+Future<String> signOutWithEmail() async {
+  await _auth.signOut();
+  return 'Sign Out';
+}
